4de66b5ad1a0288a67c0d7cd26481a4c8945017f
AnaRativa09 <anarativa09@gmail.com>
2021-10-14 11:41:44 -0500

    feat: add search bar and location button

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -6,7 +6,16 @@ import {
   InfoWindow,
 } from '@react-google-maps/api';
 import { formatRelative } from 'date-fns';
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete';
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from '@reach/combobox';
 
+import '@reach/combobox/styles.css';
 import MapStyles from '../styles/MapStyle';
 
 const libraries = ['places'];
@@ -28,7 +37,7 @@ const options = {
   zoomControl: true,
 };
 
-// Component
+// Map Component
 const Map = () => {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -53,49 +62,126 @@ const Map = () => {
     mapRef.current = map;
   }, []);
 
+  const panTo = useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(16);
+  }, []);
+
   if (loadError) return 'Error loading maps';
   if (!isLoaded) return 'Loading...';
 
   return (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      zoom={11}
-      center={center}
-      options={options}
-      onClick={onMapClick}
-      onLoad={onMapLoad}
-    >
-      {
-        newCases.map((newCase) => (
-          <Marker
-            key={newCase.time.toISOString()}
-            position={{ lat: newCase.lat, lng: newCase.lng }}
-            onClick={() => (setSelectedCase(newCase))}
-          />
-        ))
-      }
-      {
-        selectedCase
-          ? (
-            <InfoWindow
-              position={{ lat: selectedCase.lat, lng: selectedCase.lng }}
-              onCloseClick={() => (setSelectedCase(null))}
-            >
-              <div>
-                <h3>New Covid Case</h3>
-                <p>
-                  {' '}
-                  Reported:
-                  {' '}
-                  {formatRelative(selectedCase.time, new Date())}
-                </p>
-              </div>
-            </InfoWindow>
-          )
-          : null
-      }
-    </GoogleMap>
+    <section className="map-container">
+      <div className="flex-container">
+        <Search panTo={panTo} />
+        <LocateButton panTo={panTo} />
+      </div>
+
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={11}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {
+          newCases.map((newCase) => (
+            <Marker
+              key={`${newCase.lat}-${newCase.lng}`}
+              position={{ lat: newCase.lat, lng: newCase.lng }}
+              onClick={() => (setSelectedCase(newCase))}
+            />
+          ))
+        }
+        {
+          selectedCase
+            ? (
+              <InfoWindow
+                position={{ lat: selectedCase.lat, lng: selectedCase.lng }}
+                onCloseClick={() => (setSelectedCase(null))}
+              >
+                <div>
+                  <h3>New Covid Case</h3>
+                  <p>
+                    {' '}
+                    Reported:
+                    {' '}
+                    {formatRelative(selectedCase.time, new Date())}
+                  </p>
+                </div>
+              </InfoWindow>
+            )
+            : null
+        }
+      </GoogleMap>
+    </section>
   );
 };
+
+// LocateButton Component
+function LocateButton({ panTo }) {
+  return (
+    <button
+      type="button"
+      className="btn-small"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (positions) => {
+            panTo({ lat: positions.coords.latitude, lng: positions.coords.longitude });
+          },
+          () => null,
+        );
+      }}
+    >
+      <i className="far fa-compass" />
+    </button>
+  );
+}
+
+// Search Component
+function Search({ panTo }) {
+  const {
+    ready, value, suggestions: { status, data }, setValue, clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 4.6533326, lng: () => -74.083652 },
+      radius: 200 * 1000,
+    },
+  });
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  };
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => { setValue(e.target.value); }}
+        disable={!ready}
+        placeholder="Search location"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === 'OK' && data.map((place) => (
+            <ComboboxOption
+              key={place.place_id}
+              value={place.description}
+            />
+          ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+}
 
 export default Map;
