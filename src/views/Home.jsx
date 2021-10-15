0afc82c9c41f3a9df2bae9885f7d3faf33ef1902
AnaRativa09 <anarativa09@gmail.com>
2021-10-15 16:49:57 -0500

    refactor: delete unused title

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,16 +3,11 @@ import React from 'react';
 import Header from '../components/Header';
 import Map from '../components/Map';
 
-const Home = () => {
-  const viewTitle = 'Home';
-
-  return (
-    <>
-      <Header />
-      <h1>{viewTitle}</h1>
-      <Map />
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <Header />
+    <Map />
+  </>
+);
 
 export default Home;
